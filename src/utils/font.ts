ffff085464f95dfb2a72f7d4d7b5fcb2c6062db9
Josip Igrec <josip@igr.ec>
2023-04-26 12:04:02 +0200

    Add proper Dwight font

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -1,15 +1,76 @@
-import { Calistoga } from "next/font/google";
+import localFont from "next/font/local";
 
-export const fontDisplay = Calistoga({
-  subsets: ["latin-ext"],
-  weight: ["400"],
-  fallback: [
-    "ui-serif",
-    "Georgia",
-    "Cambria",
-    '"Times New Roman"',
-    "Times",
-    "serif",
+export const fontDisplay = localFont({
+  src: [
+    {
+      path: "../assets/font/Dwight/Dwight-Bold.woff2",
+      style: "normal",
+      weight: "700",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-BoldItalic.woff2",
+      style: "italic",
+      weight: "700",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-ExtraLight.woff2",
+      style: "normal",
+      weight: "200",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-ExtraLightItalic.woff2",
+      style: "italic",
+      weight: "200",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-Italic.woff2",
+      style: "italic",
+      weight: "400",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-Light.woff2",
+      style: "normal",
+      weight: "300",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-LightItalic.woff2",
+      style: "italic",
+      weight: "300",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-Medium.woff2",
+      style: "normal",
+      weight: "500",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-MediumItalic.woff2",
+      style: "italic",
+      weight: "500",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-Regular.woff2",
+      style: "normal",
+      weight: "400",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-SemiBold.woff2",
+      style: "normal",
+      weight: "600",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-SemiBoldItalic.woff2",
+      style: "italic",
+      weight: "600",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-Thin.woff2",
+      style: "normal",
+      weight: "100",
+    },
+    {
+      path: "../assets/font/Dwight/Dwight-ThinItalic.woff2",
+      style: "italic",
+      weight: "100",
+    },
   ],
-  variable: "--font-family-ui",
 });
