ad333e87ef5475448898ad38f5e4f045e92107fb
Josip Igrec <josip@igr.ec>
2023-05-24 22:53:18 +0200

    Add some button variants

--- src/components/base/button.tsx
+++ src/components/base/button.tsx
@@ -4,20 +4,29 @@ import { cn } from "~/utils/style";
 
 export type ButtonProps = HTMLProps<HTMLButtonElement> & {
   active?: boolean;
+  plain?: boolean;
+  square?: boolean;
 };
 
 export const Button: FC<PropsWithChildren<ButtonProps>> = ({
   children,
   active,
+  plain,
+  square,
+  disabled,
   ...props
 }) => {
   return (
     <button
       {...props}
+      disabled={disabled}
       type="button"
       className={cn(
-        "flex overflow-clip rounded-full border-[3px] border-black bg-white px-3 py-[1px] leading-none tracking-tight hover:bg-black hover:text-white",
+        "flex overflow-clip border-black bg-white py-[1px] leading-none tracking-tight hover:hover-hover:pointer-fine:bg-black hover:hover-hover:pointer-fine:text-white",
+        square ? "rounded-none" : "rounded-full",
+        plain ? "border-0" : "border-[3px] px-3",
         active && "bg-black text-white",
+        disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer",
         props.className,
       )}
     >
