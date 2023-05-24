8ce7627c332b466af4af8dace06fe109684b514f
Josip Igrec <josip@igr.ec>
2023-05-24 22:49:47 +0200

    Improve collapsible handling if it has no children

--- src/components/base/collapsible/index.tsx
+++ src/components/base/collapsible/index.tsx
@@ -12,19 +12,30 @@ export const Collapsible: FC<PropsWithChildren<CollapsibleProps>> = ({
   children,
   title,
 }) => {
-  return (
-    <details
-      className={cn("border-b border-b-black open:pb-3", $style.details)}
-    >
-      <summary
+  if (children) {
+    return (
+      <details
         className={cn(
-          "flex items-center justify-stretch pb-2",
-          children ? $style.withArrow : null,
+          "overflow-hidden border-b border-b-black open:pb-3",
+          $style.details,
         )}
       >
-        {title}
-      </summary>
-      {children}
-    </details>
+        <summary
+          className={cn(
+            "flex items-center justify-stretch pb-2",
+            children ? $style.withArrow : null,
+          )}
+        >
+          {title}
+        </summary>
+        {children}
+      </details>
+    );
+  }
+
+  return (
+    <div className="overflow-hidden border-b border-b-black">
+      <div className="flex items-center justify-stretch pb-2">{title}</div>
+    </div>
   );
 };
